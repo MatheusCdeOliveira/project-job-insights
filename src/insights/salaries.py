74de6e825f3a1ff583816e5c1ad1a083d0a5033d
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-04-28 15:53:04 -0300

    req 09

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -21,34 +21,27 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    if "min_salary" not in job or "max_salary" not in job:
+    try:
+        min = job["min_salary"]
+        max = job["max_salary"]
+        if int(min) > int(max):
+            raise ValueError
+        return int(min) <= int(salary) <= int(max)
+    except KeyError:
         raise ValueError
-
-    if type(job["min_salary"]) != int or type(job["max_salary"]) != int:
+    except TypeError:
         raise ValueError
-
-    if job["min_salary"] > job["max_salary"]:
-        raise ValueError
-
-    return job["min_salary"] <= int(salary) <= job["max_salary"]
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    lst_salary_filtered = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                lst_salary_filtered.append(job)
+        except ValueError:
+            print("invalid values")
+    return lst_salary_filtered
