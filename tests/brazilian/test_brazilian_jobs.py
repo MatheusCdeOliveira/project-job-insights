b627391dbe31a609dda211ba0b9851d383541b58
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-04-28 16:23:54 -0300

    req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    teste = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert "titulo" not in teste[0]
+    assert "salario" not in teste[0]
+    assert "tipo" not in teste[0]
+    assert "title" in teste[0]
+    assert "salary" in teste[0]
+    assert "type" in teste[0]
